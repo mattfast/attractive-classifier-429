19e01c14e0b0f5df41e6e3e3c753abda1a2ec9db
Akash Samant <asamant@princeton.edu>
2020-09-30 23:39:52 -0400

    info for all parts

--- a2/a2_part4_starter/test.py
+++ a2/a2_part4_starter/test.py
@@ -7,9 +7,13 @@ from find_faces import find_faces
 img = cv2.imread('face_data/testing_scenes/addams-family.jpg', cv2.IMREAD_GRAYSCALE)
 saved = np.load('face_classifier.npz')
 params, orientations, wrap180 = saved['params'], saved['orientations'], saved['wrap180']
-outimg = find_faces(img, 3, 0.95, params, orientations, wrap180)
+outimg, probmap = find_faces(img, 3, 0.93, params, orientations, wrap180)
 
 plt.figure(1)
 plt.title('outimg')
 plt.imshow(outimg, cmap='gray')
+plt.show()
+plt.figure(2)
+plt.title('probmap')
+plt.imshow(probmap, cmap='gray')
 plt.show()
