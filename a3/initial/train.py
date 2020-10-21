4984044918eedf34612306a9a5e78db08464490d
Akash Samant <asamant@princeton.edu>
2020-10-21 15:16:51 -0400

    modified train and update weights

--- a3/initial/train.py
+++ a3/initial/train.py
@@ -3,9 +3,9 @@ sys.path += ['layers']
 import numpy as np
 
 ######################################################
-# Set use_pcode to True to use the provided pyc code 
+# Set use_pcode to True to use the provided pyc code
 # for inference, calc_gradient, loss_crossentropy and update_weights
-use_pcode = False
+use_pcode = True
 
 # You can modify the imports of this section to indicate 
 # whether to use the provided pyc or your own code for each of the four functions.
@@ -46,8 +46,11 @@ def train(model, input, label, params, numIters):
     lr = params.get("learning_rate", .01)
     # Weight decay
     wd = params.get("weight_decay", .0005)
+    # Friction term
+    rho = params.get("rho", .9)
     # Batch size
     batch_size = params.get("batch_size", 128)
+
     # There is a good chance you will want to save your network model during/after
     # training. It is up to you where you save and how often you choose to back up
     # your model. By default the code saves the model in 'model.npz'.
@@ -56,21 +59,45 @@ def train(model, input, label, params, numIters):
     # update_params will be passed to your update_weights function.
     # This allows flexibility in case you want to implement extra features like momentum.
     update_params = {"learning_rate": lr, 
-                     "weight_decay": wd }
+                     "weight_decay": wd,
+                     "rho": rho}
     
     num_inputs = input.shape[-1]
     loss = np.zeros((numIters,))
+    num_layers = len(model["layers"])
+    velocity = [None,] * num_layers
+    for i in range(num_layers):
+        velocity[i]['W'] = np.zeros(model['layers'][i]['params']['W'].shape)
+        velocity[i]['b'] = np.zeros(model['layers'][i]['params']['b'].shape)
 
     for i in range(numIters):
-        # TODO: One training iteration
+        batch = np.random.choice(num_inputs, batch_size, replace=False)
+        train_inputs = input[..., batch]
+        train_labels = label[batch]
+
+        output, activations = inference(model, train_inputs)
+        curr_loss, dv_output = loss_crossentropy(output, train_labels, hyper_params=None, backprop=True)
+        loss[i] = curr_loss
+        curr_grads = calc_gradient(model, train_inputs, activations, dv_output)
+
+        for j in range(num_layers):
+            velocity[j]['W'] = (rho*velocity[j]['W'] + curr_grads[j]['W'])
+            velocity[j]['b'] = (rho*velocity[j]['b'] + curr_grads[j]['b'])
+
+        # Passing in velocity for gradient accomplishes the same update step for momentum
+        model = update_weights(model, velocity, update_params)
+        print(f"Current loss on {i}th iteration: {curr_loss}")
+
+        # Calculate and store Velocity
         # Steps:
         #   (1) Select a subset of the input to use as a batch
         #   (2) Run inference on the batch
         #   (3) Calculate loss and determine accuracy
         #   (4) Calculate gradients
         #   (5) Update the weights of the model
-        # Optionally, 
+        # Optionally,
         #   (1) Monitor the progress of training
         #   (2) Save your learnt model, using ``np.savez(save_file, **model)``
-
+    np.savez(save_file, **model)
     return model, loss
+
