f7f75bec2ae500daf0c2a13d6e89a260e7f10af7
Akash Samant <asamant@princeton.edu>
2020-12-02 15:44:43 -0700

    modifications to models, etc...

---
+++ final_project/code/test_attractive_ratings.py
@@ -0,0 +1,43 @@
+import numpy as np
+from tensorflow.keras import models
+import pandas as pd
+import cv2
+import os
+from functools import cmp_to_key
+
+
+
+def cmp(a, b):
+    a = a[1]
+    b = b[1]
+    if a > b:
+        return 1
+    elif a == b:
+        return 0
+    else:
+        return -1
+
+
+model_name = '../larger_model'
+
+img_files = []
+for path, subdirs, files in os.walk('../fairface_val'):
+    for name in files:
+        if 'jpg' not in name:
+            continue
+        num = int(name.split('.')[0])
+        img_files.append((os.path.join(path, name), num))
+img_files = sorted(img_files, key=cmp_to_key(cmp))
+img_files = [f[0] for f in img_files]
+img_files = [cv2.imread(f) for f in img_files]
+img_files = [cv2.resize(f, dsize=(250, 250)) for f in img_files]
+img_files = [image / 255 for image in img_files]
+img_files = np.array(img_files)
+
+m = models.load_model(model_name)
+
+print('Finished Model Loading')
+
+predictions = m.predict_classes(img_files)
+np.savetxt('../fair_face_predictions.csv', predictions, delimiter=',')
+
